653397e87cb5455975a419a01075006374743b38
Matt Klich <matt.klich@wayin.com>
2016-11-04 15:27:00 -0600

    PEP8 formatting, show exeption and die instead of passing and hiding errors

--- RundeckLogfileCleanup.py
+++ RundeckLogfileCleanup.py
@@ -7,6 +7,7 @@ import json
 import sys
 import traceback
 
+
 # Returns list of all the project names
 def get_projects():
     global PROPERTIES
@@ -14,15 +15,18 @@ def get_projects():
     project_names = []
     try:
         url = URL + 'projects'
-        r = requests.get(url, headers=HEADERS, verify=False,timeout=PROPERTIES['TIMEOUT'])
+        r = requests.get(
+            url, headers=HEADERS, verify=False, timeout=PROPERTIES['TIMEOUT'])
         root = ET.fromstring(r.text.encode('utf-8'))
-        for project in root:	
+        for project in root:
             for name in project.findall('name'):
                 project_names.append(name.text)
-    except:
-        print "Problem with project listing {0}".format(r)
-        pass
-    return project_names   
+    except Exception as e:
+        print "Problem with project listing"
+        print e.message
+        raise e
+    return project_names
+
 
 # Returns list of all the jobids
 def get_jobs_for_project(project_name):
@@ -31,29 +35,44 @@ def get_jobs_for_project(project_name):
     job_ids = []
     try:
         url = URL + 'jobs'
-        payload = { 'project':  project_name }
-        r = requests.get(url, params=payload, headers=HEADERS, verify=False,timeout=PROPERTIES['TIMEOUT'])    
-        root = ET.fromstring(r.text.encode('utf-8'))	
+        payload = {'project': project_name}
+        r = requests.get(url,
+                         params=payload,
+                         headers=HEADERS,
+                         verify=False,
+                         timeout=PROPERTIES['TIMEOUT'])
+        root = ET.fromstring(r.text.encode('utf-8'))
         for job in root:
-            job_ids.append( (job.attrib['id'],job.find('name').text) )
-    except:
-        print "Problem with job listing {0}".format(r)
-        pass
+            job_ids.append((job.attrib['id'], job.find('name').text))
+    except Exception as e:
+        print "Problem with job listing"
+        print e.message
+        raise e
     return job_ids
 
-# API call to get a page of the executions for a particular job id      
-def get_executions_for_job(job_id,page):
+
+# API call to get a page of the executions for a particular job id
+def get_executions_for_job(job_id, page):
     global PROPERTIES
     global HEADERS
     root = None
     try:
-        url = URL + 'job/'+job_id+'/executions'
-        r = requests.get(url, params={'max':PROPERTIES['PAGE_SIZE'],'offset':page*PROPERTIES['PAGE_SIZE']}, headers=HEADERS, verify=False,timeout=PROPERTIES['TIMEOUT'])
+        url = URL + 'job/' + job_id + '/executions'
+        r = requests.get(url,
+                         params={
+                             'max': PROPERTIES['PAGE_SIZE'],
+                             'offset': page * PROPERTIES['PAGE_SIZE']
+                         },
+                         headers=HEADERS,
+                         verify=False,
+                         timeout=PROPERTIES['TIMEOUT'])
         root = ET.fromstring(r.text.encode('utf-8'))
-    except:
-        print "Problem with execution listing {0}".format(r)
-        pass
+    except Exception as e:
+        print "Problem with execution listing"
+        print e.message
+        raise e
     return root
+
 
 #
 # Return a dictionary of execute ids & dates
@@ -69,18 +88,23 @@ def get_execution_dates(root):
     except:
         pass
     return execid_dates
-       
+
+
 #API call to delete an execution by ID
 def delete_execution(execution_id):
     global PROPERTIES
     global HEADERS
-    url = URL + 'execution/'+execution_id
+    url = URL + 'execution/' + execution_id
     try:
-        r = requests.delete(url, headers=HEADERS, verify=False,timeout=PROPERTIES['TIMEOUT'])
+        r = requests.delete(
+            url, headers=HEADERS, verify=False, timeout=PROPERTIES['TIMEOUT'])
         if PROPERTIES['VERBOSE']:
-            print "            Deleted execution id {0} {1} {2}".format( execution_id, r.text, r )
-    except:
-        pass
+            print "            Deleted execution id {0} {1} {2}".format(
+                execution_id, r.text, r)
+    except Exception as e:
+        print e.message
+        raise e
+
 
 #API call to bulk delete executions by ID
 def delete_executions(execution_ids):
@@ -88,66 +112,85 @@ def delete_executions(execution_ids):
     global HEADERS
     url = URL + 'executions/delete'
     try:
-        r = requests.post(url, headers=HEADERS, data= json.dumps({'ids':execution_ids}) , verify=False,timeout=PROPERTIES['DELETE_TIMEOUT'])
+        r = requests.post(
+            url,
+            headers=HEADERS,
+            data=json.dumps({
+                'ids': chunk
+            }),
+            verify=False,
+            timeout=PROPERTIES['DELETE_TIMEOUT'])
         if PROPERTIES['VERBOSE']:
-            print "            Deleted execution ids {0}".format( execution_ids, r.text, r )
-    except:
-        try:
-            print "Problem with execution deletion {0}".format(r)
-        except:
-            pass
-        pass
-    
+            print "            Deleted execution ids {0}".format(execution_ids,
+                                                                 r.text, r)
+    except Exception as e:
+        print "Problem with execution deletion"
+        print e.message
+        raise e
+
+
 def delete_test(execution_date):
     global PROPERTIES
     global TODAY
-    millis_in_one_day = 1000*60*60*24
-    return ((TODAY - execution_date) > millis_in_one_day * PROPERTIES['MAXIMUM_DAYS'])
+    millis_in_one_day = 1000 * 60 * 60 * 24
+    return ((TODAY - execution_date) > millis_in_one_day *
+            PROPERTIES['MAXIMUM_DAYS'])
+
 
 def check_deletion(execid_dates):
-    delete= ()
+    delete = ()
     for exec_id, exec_date in execid_dates.iteritems():
-        if delete_test( int(exec_date) ):
-            delete  += (exec_id,)
-    print "        Delete {0} jobs from this page".format( len(delete) )
+        if delete_test(int(exec_date)):
+            delete += (exec_id, )
+    print "        Delete {0} jobs from this page".format(len(delete))
     return delete
+
 
 #
 # Main
 #
-setting_filename = sys.argv[1] if len(sys.argv)>1 else 'properties.json'
-with open(setting_filename,'r') as props_file:    
-    PROPERTIES = json.load( props_file )
+setting_filename = sys.argv[1] if len(sys.argv) > 1 else 'properties.json'
+with open(setting_filename, 'r') as props_file:
+    PROPERTIES = json.load(props_file)
 
-protocol='http'
+protocol = 'http'
 if PROPERTIES['SSL']:
-	protocol='https'
-	# disable warnings about unverified https connections
-	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    protocol = 'https'
+    # disable warnings about unverified https connections
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-URL = '{0}://{1}:{2}/api/{3}/'.format(protocol,PROPERTIES['RUNDECKSERVER'],PROPERTIES['PORT'],PROPERTIES['API_VERSION'])
-HEADERS = {'Content-Type': 'application/json','X-RunDeck-Auth-Token': PROPERTIES['API_KEY'] }
+URL = '{0}://{1}:{2}/rundeck/api/{3}/'.format(
+    protocol, PROPERTIES['RUNDECKSERVER'], PROPERTIES['PORT'],
+    PROPERTIES['API_VERSION'])
+HEADERS = {
+    'Content-Type': 'application/json',
+    'X-RunDeck-Auth-Token': PROPERTIES['API_KEY']
+}
 
 TODAY = int(round(time.time() * 1000))
 
 for project in get_projects():
     print project
-    for (jobid,jobname) in get_jobs_for_project(project):
+    for (jobid, jobname) in get_jobs_for_project(project):
         print "    {0}".format(jobname.encode('utf-8'))
         page = 0
         deleteable = ()
         more = True
-        while ( more and len(deleteable)< PROPERTIES['MAX_DELETE'] ) :
+        while (more and len(deleteable) < PROPERTIES['MAX_DELETE']):
             try:
-                execution_root = get_executions_for_job(jobid,page)
-                print "        Page {0} got {1} jobs".format(page,execution_root.attrib['count'])
+                execution_root = get_executions_for_job(jobid, page)
+                print "        Page {0} got {1} jobs".format(
+                    page, execution_root.attrib['count'])
                 page += 1
-                deleteable += check_deletion(get_execution_dates(execution_root))
-                more = int(execution_root.attrib['count']) == PROPERTIES['PAGE_SIZE']
+                deleteable += check_deletion(
+                    get_execution_dates(execution_root))
+                more = int(execution_root.attrib['count']) == PROPERTIES[
+                    'PAGE_SIZE']
             except:
-                print  traceback.format_exc()
-                print "Problem with executions {0} {1}".format(execution_root,sys.exc_info()[0])
+                print traceback.format_exc()
+                print "Problem with executions {0} {1}".format(
+                    execution_root, sys.exc_info()[0])
                 more = False
-        if (len(deleteable) > 0 ):
-            print "        Deleting {0} jobs in total".format( len(deleteable) )
+        if (len(deleteable) > 0):
+            print "        Deleting {0} jobs in total".format(len(deleteable))
             delete_executions(deleteable)
